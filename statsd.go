d776fc8f15f777558cc384c4e8fcd7e9baf9e8b2
dmiles <dmiles@monsooncommerce.com>
2014-05-23 16:57:13 -0700

    initial commit

---
+++ statsd.go
@@ -0,0 +1,78 @@
+package stats
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/cactus/go-statsd-client/statsd"
+	"github.com/monsooncommerce/log"
+)
+
+type statsToBeIncremented struct {
+	Stat  string
+	Count int64
+	Rate  float32
+}
+type statsToBeTraced struct {
+	Stat  string
+	Start time.Time
+}
+type StatsClientWrapper struct {
+	Client             *statsd.Client
+	IncrementableStats map[string]statsToBeIncremented
+	TimeableStats      map[string]statsToBeTraced
+}
+
+func CreateStatsdClient() (*StatsClientWrapper, error) {
+	address := os.Getenv("STATSD_ADDRESS")
+	if address == "" {
+		log.Error("environment variable STATSD_ADDRESS not defined, cannot continue")
+		os.Exit(1)
+	}
+	prefix := os.Getenv("STATSD_PREFIX")
+	if prefix == "" {
+		log.Error("environment variable STATSD_PREFIX not defined, cannot continue")
+		os.Exit(1)
+	}
+	client, err := statsd.New(address, prefix)
+	if err != nil {
+		log.Error("Couldn't initialize statsd.  StatsdInitError=\"" + err.Error() + "\"")
+		os.Exit(1)
+	}
+	wrapper := StatsClientWrapper{client, make(map[string]statsToBeIncremented), make(map[string]statsToBeTraced)}
+	return &wrapper, err
+}
+func (w *StatsClientWrapper) StartTrace(traceIdentifier string) error {
+	timestamp := time.Now()
+	w.TimeableStats[traceIdentifier] = statsToBeTraced{traceIdentifier, timestamp}
+	return nil
+}
+
+func (w *StatsClientWrapper) EndTrace(traceIdentifier string) error {
+	timestamp := time.Now()
+	_, keyExists := w.TimeableStats[traceIdentifier]
+	if !keyExists {
+		return errors.New(fmt.Sprintf("Error, must start trace \"%s\" before ending it", traceIdentifier))
+	}
+	startTime := w.TimeableStats[traceIdentifier].Start
+	duration := int64(timestamp.Sub(startTime))
+	w.Client.Timing(traceIdentifier, duration, 1)
+	delete(w.TimeableStats, traceIdentifier)
+	return nil
+}
+
+func (w *StatsClientWrapper) Inc(stat string) error {
+	_, keyExists := w.IncrementableStats[stat]
+	if !keyExists {
+		w.IncrementableStats[stat] = statsToBeIncremented{stat, 0, 1.0}
+	}
+	count := w.IncrementableStats[stat].Count + 1
+	oldRate := w.IncrementableStats[stat].Rate
+	// BOZO: cannot assign to w.IncrementableStats[stat].Count, so replace with
+	//       totally new object and hope the garbage collector does its thing
+	w.IncrementableStats[stat] = statsToBeIncremented{stat, count, oldRate}
+	w.Client.Inc(stat, count, oldRate)
+	return nil
+}
---
+++ statsd_test.go
@@ -0,0 +1,101 @@
+package stats
+
+import (
+	"fmt"
+	"math"
+	"net"
+	"os"
+	"regexp"
+	"strconv"
+	"testing"
+	"time"
+
+	. "github.com/franela/goblin"
+	. "github.com/onsi/gomega"
+)
+
+func TestStatsd(t *testing.T) {
+	g := Goblin(t)
+	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
+	g.Describe("Sanity", func() {
+		g.It("tests should function in a sane environment", func() {
+			Expect(1).To(Equal(1))
+		})
+	})
+	g.Describe("statsd", func() {
+		buf := make([]byte, 1024)
+		var addr *net.UDPAddr
+		var sock *net.UDPConn
+		var err error
+		g.BeforeEach(func() {
+			addr = nil
+			sock = nil
+			addr, err = net.ResolveUDPAddr("udp", "127.0.0.1:31337")
+			sock, err = net.ListenUDP("udp", addr)
+			os.Setenv("STATSD_ADDRESS", "127.0.0.1:31337")
+			os.Setenv("STATSD_PREFIX", "test")
+		})
+		g.AfterEach(func() {
+			if sock != nil {
+				sock.Close()
+			}
+		})
+		g.It("should correctly initialize", func() {
+			client, err := CreateStatsdClient()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(client).NotTo(Equal(nil))
+		})
+		g.It("should send and track increment", func() {
+			client, err := CreateStatsdClient()
+			Expect(err).NotTo(HaveOccurred())
+			for i := 0; i < 5; i++ {
+				err = client.Inc("teststat")
+				Expect(err).NotTo(HaveOccurred())
+				readLength, _, err := sock.ReadFromUDP(buf)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(readLength).To(Equal(17))
+				Expect(string(buf[:readLength])).To(Equal(fmt.Sprintf("test.teststat:%d|c", i+1)))
+			}
+		})
+		g.It("should send accurate timing", func() {
+			client, err := CreateStatsdClient()
+			Expect(err).NotTo(HaveOccurred())
+			err = client.StartTrace("testing trace")
+			Expect(err).NotTo(HaveOccurred())
+			time.Sleep(1 * time.Millisecond)
+			err = client.EndTrace("testing trace")
+			Expect(err).NotTo(HaveOccurred())
+			readLength, _, err := sock.ReadFromUDP(buf)
+			Expect(err).NotTo(HaveOccurred())
+			readStr := string(buf[:readLength])
+			// match something like: "test.testing trace:1139292|ms"
+			re := regexp.MustCompile(`(.*)\.(.*):([0-9]*)\|(.*)`)
+			match := re.FindStringSubmatch(readStr)
+			Expect(len(match)).To(Equal(5))
+			Expect(match[1]).To(Equal("test"))
+			Expect(match[2]).To(Equal("testing trace"))
+			Expect(match[4]).To(Equal("ms"))
+			traceTime, err := strconv.ParseInt(match[3], 10, 64)
+			Expect(err).NotTo(HaveOccurred())
+			driftNanoseconds := math.Abs(float64(traceTime - int64(time.Millisecond)))
+			Expect(driftNanoseconds).Should(BeNumerically("<", 10*time.Millisecond))
+		})
+		g.It("should provide correct error when ending a trace without starting it", func() {
+			client, err := CreateStatsdClient()
+			Expect(err).NotTo(HaveOccurred())
+			err = client.EndTrace("testing trace")
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("Error, must start trace \"testing trace\" before ending it"))
+		})
+		g.It("should clean up completed traces", func() {
+			client, err := CreateStatsdClient()
+			Expect(err).NotTo(HaveOccurred())
+			err = client.StartTrace("testing trace")
+			Expect(err).NotTo(HaveOccurred())
+			err = client.EndTrace("testing trace")
+			Expect(err).NotTo(HaveOccurred())
+			_, foundTestingTrace := client.TimeableStats["testing trace"]
+			Expect(foundTestingTrace).To(Equal(false))
+		})
+	})
+}
